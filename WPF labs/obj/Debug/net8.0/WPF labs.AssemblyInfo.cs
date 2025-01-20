1a51d05b2d083e21bd75e19fc4c06831881e08b8
Михаил Чернецов <mikhailcher2006@gmail.com>
2025-01-20 17:45:29 +0300

    Lab1

--- WPF labs/obj/Debug/net8.0/WPF labs.AssemblyInfo.cs
+++ WPF labs/obj/Debug/net8.0/WPF labs.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WPF labs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39dddd20ab0bcf2d8522b43d190cb374fc83fc96")]
 [assembly: System.Reflection.AssemblyProductAttribute("WPF labs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WPF labs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
