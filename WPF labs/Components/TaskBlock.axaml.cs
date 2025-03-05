466d44e8318902fe42652a6ada5883da2c752d84
Михаил Чернецов <mikhailcher2006@gmail.com>
2025-03-05 09:50:20 +0300

    Lab4

---
+++ WPF labs/Components/TaskBlock.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace WPF_labs.Components;
+
+public partial class TaskBlock : UserControl
+{
+    public TaskBlock()
+    {
+        InitializeComponent();
+    }
+}
